d48f17ba025cf53f82bf0968e0de5f6f6f32bdec
Caio Lima <caiolima86@gmail.com>
2024-04-02 04:52:51 -0300

    mudou nome do index

--- index.js
+++ index.js
@@ -2,60 +2,211 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.type('html').send(html));
+app.get("/", (req, res) => res.type('html').send("oi"));
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
 
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
+const puppeteer = require("puppeteer");
+const { MongoClient } = require('mongodb');
+const uri = 'mongodb://127.0.0.1:27017';
+const client = new MongoClient(uri);
+const database = client.db("futebol-virtual-betano");
+let db = database.collection("futebol-virtual-betano");
+//var jogos = await db.find({}).limit(0).sort({_id: 1}).skip(0).toArray();
+
+let browser, page;
+
+let dadosJogos = {};
+
+
+async function robo() {
+
+        try {
+
+                browser = await puppeteer.launch({headless: false});
+                page = await browser.newPage();
+                await page.setViewport({ width: 1550, height: 740});
+
+                // Navigate the page to a URL
+                await page.goto('https://br.betano.com/virtuals/', {
+                        waitUntil: 'networkidle0',
+                });
+
+                roboCarregaPaginaJogo();
+
+        } catch (err) {
+                console.error("ERRO 1", err);
+        }
+
+}
+
+async function roboCarregaPaginaJogo(){
+        try{
+
+                // fecha login
+                if (await page.$("#landing-page-modal > div > div.sb-modal__close > button")) {
+                        await page.click("#landing-page-modal > div > div.sb-modal__close > button");
+                }
+
+                // fecha avisso cookie
+                if (await page.$("#app > div > div > section.main-content-wrapper > div.sticky-notification.sticky-notification--bottom > div > div.sticky-notification__actions-container > button:nth-child(2)")) {
+                        await page.click("#app > div > div > section.main-content-wrapper > div.sticky-notification.sticky-notification--bottom > div > div.sticky-notification__actions-container > button:nth-child(2)");
+                }
+
+                // remove setas para rolar os jogos que bugam
+                let el = await page.waitForSelector("#virtuals-upcoming-events-next");
+                await el.evaluate(el => el.remove());
+                el = await page.waitForSelector("#virtuals-upcoming-events-prev");
+                await el.evaluate(el => el.remove());
+
+                // busca listagem de jogos futuros
+                //await buscaJogosFuturos();
+
+                // busca resultados dos jogos passados
+                await buscaResultadosJogos();
+
+
+        } catch (error) {
+                console.log("ERRO GENERICO", error);
+                //await roboCarregaPaginaJogo();
+        }
+}
+
+async function buscaResultadosJogos(){
+
+        await delay(2000);
+        await page.waitForSelector('#app > div > div > section.main-content-wrapper > div.grid__row.main-content-wrapper__content > div.grid__column.grid__column--fluid.grid__column__main-content > section > section > section > div > div.virtuals-stream-container__wrapper > div.tw-relative.tw-flex.tw-items-center.tw-w-full > div.tw-flex.tw-flex-col.tw-items-center.tw-justify-center.tw-rounded-s.tw-cursor-pointer.tw-transition-colors.tw-duration-base.tw-bg-n-17-black-pearl.tw-w-\\[60px\\].tw-h-\\[66px\\].tw-ml-n');
+        let btn = await page.$('#app > div > div > section.main-content-wrapper > div.grid__row.main-content-wrapper__content > div.grid__column.grid__column--fluid.grid__column__main-content > section > section > section > div > div.virtuals-stream-container__wrapper > div.tw-relative.tw-flex.tw-items-center.tw-w-full > div.tw-flex.tw-flex-col.tw-items-center.tw-justify-center.tw-rounded-s.tw-cursor-pointer.tw-transition-colors.tw-duration-base.tw-bg-n-17-black-pearl.tw-w-\\[60px\\].tw-h-\\[66px\\].tw-ml-n');
+        await btn.click();
+
+        await delay(5000);
+
+        await page.waitForSelector('#app > div > div > section.main-content-wrapper > div.grid__row.main-content-wrapper__content > div.grid__column.grid__column--fluid.grid__column__main-content > section > section > section > div > div.tw-flex.tw-flex-col.tw-bg-n-8-dark-steel.tw-rounded-s.tw-m-n.tw-pb-m > div');
+        let listaResultadosJogosArray = await page.$$('#app > div > div > section.main-content-wrapper > div.grid__row.main-content-wrapper__content > div.grid__column.grid__column--fluid.grid__column__main-content > section > section > section > div > div.tw-flex.tw-flex-col.tw-bg-n-8-dark-steel.tw-rounded-s.tw-m-n.tw-pb-m > div')
+
+        for (const el of listaResultadosJogosArray) {
+                if(await (await el.getProperty('className')).jsonValue() === ""){
+                        //console.log(el);
+                        let elHora = await el.$$('span');
+                        let horario = await elHora[0].evaluate(el3 => el3.textContent, elHora[0]);
+                        //console.log("horario", horario);
+
+                        let elDados = await el.$$('div');
+                        //console.log("elDados", elDados);
+                        let time1 = await elDados[2].evaluate(el3 => el3.textContent, elDados[2]);
+                        //console.log("time1", time1);
+
+                        let time2 = await elDados[6].evaluate(el3 => el3.textContent, elDados[6]);
+                        //console.log("time2", time2);
+
+                        let resultadoFinal = await elDados[4].evaluate(el3 => el3.textContent, elDados[4]);
+                        //console.log("resultadoFinal", resultadoFinal);
+
+                        let resultadoPrimeiroTempo = await elDados[5].evaluate(el3 => el3.textContent, elDados[5]);
+                        resultadoPrimeiroTempo = resultadoPrimeiroTempo.replace(" (", "").replace(") ", "");
+                        //console.log("resultadoPrimeiroTempo", resultadoPrimeiroTempo);
+
+                        if(dadosJogos[horario] !== undefined){
+                                dadosJogos[horario]["resultadoFinal"] = resultadoFinal;
+                                dadosJogos[horario]["resultadoPrimeiroTempo"] = resultadoPrimeiroTempo;
+                                dadosJogos[horario]["dataUpd"] = new Date().toISOString();
+                                console.log("ATUALIZADO ==============================");
+                                console.dir(dadosJogos, { depth: null });
+
+                                // adiciona registro no DB
+                                /*try{
+                                        await db.insertOne(dadosJogos[horario]);
+                                        console.log("ADICIONADO NO DB "+horario);
+
+                                        delete dadosJogos[horario];
+                                        console.dir(dadosJogos, { depth: null });
+
+                                } catch (error){
+                                        console.log(error);
+                                }*/
+                        }
+                }
+        }
+
+        await delay(2000);
+        await buscaJogosFuturos();
+}
+
+
+async function buscaJogosFuturos(){
+
+        await page.waitForSelector('#app > div > div > section.main-content-wrapper > div.grid__row.main-content-wrapper__content > div.grid__column.grid__column--fluid.grid__column__main-content > section > section > section > div > div.virtuals-stream-container__wrapper > div.tw-relative.tw-flex.tw-items-center.tw-w-full > div.virtuals-swiper-wrapper > div > div.swiper-wrapper > div.swiper-slide');
+        let listaProximosJogosArray = await page.$$('#app > div > div > section.main-content-wrapper > div.grid__row.main-content-wrapper__content > div.grid__column.grid__column--fluid.grid__column__main-content > section > section > section > div > div.virtuals-stream-container__wrapper > div.tw-relative.tw-flex.tw-items-center.tw-w-full > div.virtuals-swiper-wrapper > div > div.swiper-wrapper > div.swiper-slide')
+
+        for (const el of listaProximosJogosArray) {
+                // remove o primeiro jogo pq ja esta rolando
+                if(await (await el.getProperty('className')).jsonValue() !== "swiper-slide swiper-slide-active"){
+                        await el.click();
+                        await delay(5000);
+
+                        let info = await el.$$('.tw-leading-s');
+
+                        let time1 = await info[0].evaluate(el3 => el3.textContent, info[0]);
+                        time1 = time1.slice(0, -1);
+                        let time2 = await info[1].evaluate(el3 => el3.textContent, info[1]);
+                        let horario = await info[2].evaluate(el3 => el3.textContent, info[2]);
+
+                        console.log("horario", horario);
+                        console.log("time 1", time1);
+                        console.log("time 2", time2);
+
+                        // verifica se ja existe o jogo cadastrado
+                        if(dadosJogos[horario] === undefined){
+                                dadosJogos[horario] = {"time1": time1, "time2": time2, "dataAdd": new Date().toISOString()}
+
+                                /*console.log("dadosJogos");
+                                console.dir(dadosJogos, { depth: null });*/
+
+                                let odds = {};
+
+                                let listaDadosJogoArray = await page.$$('div.markets__market');
+                                for (const el2 of listaDadosJogoArray) {
+
+                                        let header = await el2.$$('div.markets__market__header__title > span');
+                                        let txtHeader = await header[0].evaluate(el3 => el3.textContent, header[0]);
+                                        txtHeader = txtHeader.replace(time1, "Time 1").replace(time2, "Time 2");
+                                        console.log("header", txtHeader);
+                                        odds[txtHeader] = {}
+
+                                        let listaOddsJogoArray = await el2.$$('div.selections > button');
+                                        for (const el2 of listaOddsJogoArray) {
+                                                let textoBotao = await page.evaluate(el3 => el3.getAttribute("aria-label").slice(0, -1), el2);
+                                                //console.log("texto botao", textoBotao);
+
+                                                let regex = new RegExp("Bet on (.+) with odds (.+)").exec(textoBotao);
+                                                let txt = regex[1]
+                                                let odd = parseFloat(regex[2]);
+                                                //console.log("RegExp", new RegExp("Bet on (.+) with odds (.+)").exec(textoBotao));
+                                                //console.log("odd", odd);
+                                                //console.log("texto", txt);
+                                                odds[txtHeader][txt] = odd;
+                                        }
+                                }
+
+                                dadosJogos[horario]["odds"] = odds;
+                                console.log("dadosJogos2");
+                                console.dir(dadosJogos, { depth: null });
+                        }
+
+
+
+                }
+
+        }
+
+        await buscaResultadosJogos();
+}
+
+function delay(time) {
+        return new Promise(function(resolve) {
+                setTimeout(resolve, time)
         });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`
+}
